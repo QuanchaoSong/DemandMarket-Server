dcd9caba81524dafb6aff975cf3105ea4b3cd1a2
AlbusSong <strawberry1234561@gmail.com>
2020-05-06 16:06:30 +0300

    微信开发者AppID

--- Sources/App/Others/Macro/MacroOfConstant.swift
+++ Sources/App/Others/Macro/MacroOfConstant.swift
@@ -11,3 +11,7 @@ import Foundation
 let VERSION: String = "v1.0"
 
 let JWT_SIGN_SECRET: String = "Khaa7ich_y9bE4sdn"
+
+// 微信开发者
+let WX_APP_ID: String = "wx409f76ea29ad5a04"
+let WX_APP_SECRET: String = "3fb7da4ae0ca6352a585671788c88bc8"
